ecc33b95a8d31bdcc33a89e6e9ed17bb14bfd943
recamadasj <jrecamadas@stackify.com>
2020-03-26 14:22:33 +0800

    Added new testing for GET University Module

---
+++ Users-Demo.Tests/University/UniversityTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Users_Demo.Tests.University
+{
+    public class UniversityTests : IClassFixture<TestingFactory<Startup>>
+    {
+        private readonly TestingFactory<Startup> _factory;
+
+        public UniversityTests(TestingFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+
+        [Theory]
+        [InlineData("api/university")]
+        public async Task Get_University_Return_Success(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            var code = response.EnsureSuccessStatusCode();
+
+            Assert.Equal("OK", response.ReasonPhrase);
+            Assert.True(code.IsSuccessStatusCode);
+        }
+
+        [Theory]
+        [InlineData("api/universities")] //wrong route
+        public async Task Get_UniversityById_Return_Success(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            var statusCode = response.StatusCode;
+
+            Assert.Equal("Not Found", response.ReasonPhrase);
+            Assert.Equal(HttpStatusCode.NotFound, statusCode);
+        }
+
+
+    }
+}
---
+++ Users-Demo.Tests/Utilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Users_Demo.DAL;
+using Users_Demo.DAL.Models;
+
+namespace Users_Demo.Tests
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(UsersDemoContext db)
+        {
+            db.Users.AddRange(AddUsers());
+            db.University.AddRange(AddUniversities());
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(UsersDemoContext db)
+        {
+            
+            db.Users.RemoveRange(db.Users);
+            db.University.RemoveRange(db.University);
+            InitializeDbForTests(db);
+        }
+
+        public static List<Users> AddUsers()
+        {
+            return new List<Users>()
+            {
+                new Users(){ FirstName = "TestF1", LastName = "TestL1", IsDeleted = false, DateOfBirth = DateTime.UtcNow, IsActive = true },
+                new Users(){ FirstName = "TestF2", LastName = "TestL2", IsDeleted = false, DateOfBirth = DateTime.UtcNow, IsActive = true },
+                new Users(){ FirstName = "TestF3", LastName = "TestL2", IsDeleted = false, DateOfBirth = DateTime.UtcNow, IsActive = true },
+            };
+        }
+
+        public static List<DAL.Models.University> AddUniversities()
+        {
+            return  new List<DAL.Models.University>()
+            {
+                new DAL.Models.University(){ Id = 1, IsDeleted = false, Name = "Test1", IsActive = true },
+                new DAL.Models.University(){ Id = 2, IsDeleted = false, Name = "Test2", IsActive = true }
+            };
+        }
+    }
+}
